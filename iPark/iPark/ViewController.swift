8b9b242000c949cbadea55fd2a85a9efc3ad6dd8
Luis Antonio Sánchez Pérez <0161624@imac-de-administrador.local>
2018-04-03 19:06:43 -0500

    My initial proyect setup

---
+++ iPark/iPark/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iPark
+//
+//  Created by Luis Antonio Sánchez Pérez on 03/04/18.
+//  Copyright © 2018 Luis Antonio Sánchez Pérez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
