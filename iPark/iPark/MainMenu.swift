e9e84a64be2a295dfac147180490cd3cb5533581
Luis Antonio Sánchez Pérez <0161624@imac-de-administrador.local>
2018-04-03 21:07:28 -0500

    Vista de Menú Completa Con todos los Iconos (Segunda Vista Completa)

---
+++ iPark/iPark/MainMenu.swift
@@ -0,0 +1,35 @@
+//
+//  MainMenu.swift
+//  iPark
+//
+//  Created by Luis Antonio Sánchez Pérez on 03/04/18.
+//  Copyright © 2018 Luis Antonio Sánchez Pérez. All rights reserved.
+//
+
+import UIKit
+
+class MainMenu: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
