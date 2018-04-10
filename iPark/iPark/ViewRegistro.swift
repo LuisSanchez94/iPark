c7b7f365d5508e4b0298e925eca2cd441b0a513d
Luis Antonio Sánchez Pérez <0161624@imac-de-administrador.local>
2018-04-10 18:53:37 -0500

    Se creo vista de registro - se agregaron los constrait (Completo)

---
+++ iPark/iPark/ViewRegistro.swift
@@ -0,0 +1,35 @@
+//
+//  ViewRegistro.swift
+//  iPark
+//
+//  Created by Luis Antonio Sánchez Pérez on 10/04/18.
+//  Copyright © 2018 Luis Antonio Sánchez Pérez. All rights reserved.
+//
+
+import UIKit
+
+class ViewRegistro: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
